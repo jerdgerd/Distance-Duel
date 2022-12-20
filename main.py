92e84fcd9bf8835055c7ee51acb0c49a688af969
jerdgerd <jstanton32@gatech.edu>
2022-12-20 18:35:41 -0500

    Added clearer formatting, substring matching, and fixed an off-by-one error

--- main.py
+++ main.py
@@ -111,7 +111,10 @@ def getContinent(country):
 def listSearch(cityName):
   locationMatches = []
   for city in cities:
-    if cityName == city[0].lower():
+    if (len(cityName) >= 5):
+      if (cityName in city[0].lower()):
+        locationMatches.append(city)
+    elif cityName == city[0].lower():
       locationMatches.append(city)
   return locationMatches
 
@@ -121,14 +124,16 @@ def collectCities(cityName):
   if len(cityMatches) == 0:
     return None
   if (len(cityMatches) == 1):
+    print(f"------> You selected {cityMatches[0][0]}, {cityMatches[0][3]}")
     return cityMatches[0]
   for i in range(1, len(cityMatches) + 1):
     print(
       f"{i}: {cityMatches[i- 1][0]}, {cityMatches[i-1][3]}, with a population of {cityMatches[i-1][4]}"
     )
   selection = ""
-  while not selection.isdigit() or not int(selection) < len(cityMatches) or not int(selection) > 0:
+  while not selection.isdigit() or not int(selection) < len(cityMatches) + 1 or not int(selection) > 0:
     selection = input("Please select a match from the list above, using only the number given: ")
+  print(f"------> You selected {cityMatches[int(selection) - 1][0]}, {cityMatches[int(selection) - 1][3]}")
   return cityMatches[int(selection) - 1]
 
 
@@ -168,13 +173,15 @@ def main():
   while (len(name) != 3):
     name = input("Enter your 3-letter initials here: ")
   for count in range(0, numTries):
-    print("You are on try " + str(count+1) + " of " + str(numTries))
+    print("You are on duel " + str(count+1) + " of " + str(numTries))
     city1, city2 = cityPicker(difficultyCities)
     continent1 = countriesToContinents.get(city1[3], "ERROR1")
     continent2 = countriesToContinents.get(city2[3], "ERROR2")
+    print("====================================================================================================================")
     print(
       f"What are two cities that are approximately the same distance apart as {city1[0]}, {city1[3]} and {city2[0]}, {city2[3]}?"
     )
+    print("====================================================================================================================")
     city3, city4 = None, None
     while (city3 == None or city4 == None):
       cityName = (input("First city: ")).lower()
@@ -182,9 +189,9 @@ def main():
       cityName = (input("Second city: ")).lower()
       city4 = collectCities(cityName)
       if (city3 == None or city4 == None):
-        print("One of your cities is not in our database. This may be because the name of the city is misspelled, or because we are using a different name of the city. Please try again.")
+        print("------> ERROR: One of your cities is not in our database. This may be because the name of the city is misspelled, or because we are using a different name of the city. Please try again.")
       elif (not validContinent(continent1, continent2, city3) or not validContinent(continent1, continent2, city4)):
-        print("One of your cities is on the same continent as a city provided in the question. Please try again.")
+        print("------> ERROR: One of your cities is on the same continent as a city provided in the question. Please try again.")
         city3 = None
     distance1 = distance(city1[1], city1[2], city2[1], city2[2])
     distance2 = distance(city3[1], city3[2], city4[1], city4[2])
@@ -196,6 +203,7 @@ def main():
       )
     addedScore = addToScore(distance1, diff)
     score = score + addedScore
+    print("====================================================================================================================")
     print("You scored " + str(addedScore) +" this round!")
     print("Your new total score is "  + str(score))
     if (addedScore > complimentValue):
@@ -204,8 +212,8 @@ def main():
     elif (addedScore < insultValue):
       randomIndex = random.randint(0, len(insults) - 1)
       print(insults[randomIndex])
+    print("====================================================================================================================")
     print("\n")
-  print("=============================")
   print("Your final score is " + str(score))
   with open('data/highScores.csv', 'a') as file:
     print(f"{name}, {score}", file=file)
